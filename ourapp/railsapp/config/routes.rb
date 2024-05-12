45eedd42c5b200ae11d73c8aca18f7927ad9990e
Alex <akp42@duke.edu>
2024-05-12 15:46:25 -0400

    init

--- ourapp/railsapp/config/routes.rb
+++ ourapp/railsapp/config/routes.rb
@@ -1,14 +1,14 @@
+# This is the end of the Rails application routes configuration.
+# It marks the end of the block where routes are defined using the `draw` method.
+# Any additional code should be placed after this line.
 Rails.application.routes.draw do
-  # Define all RESTful resources
-
-  # Define the routes for the API
-
-
-
-  # !Note from Alex: Keep this code if you want to work with the Sidekiq Web UI
-  # What is Sidekiq? Sidekiq is a background processing framework for Ruby. It uses threads to handle many jobs at the same time in the same process. It does not require Rails but will integrate tightly with Rails 3/4/5 to make background processing dead simple.  
-  # For Sidekiq Web UI
-  require "sidekiq/web"
-  mount Sidekiq::Web => "/sidekiq"
-
+  namespace :api do
+    resources :users
+  end
 end
+
+# !Note from Alex: Keep this code if you want to work with the Sidekiq Web UI
+# What is Sidekiq? Sidekiq is a background processing framework for Ruby. It uses threads to handle many jobs at the same time in the same process. It does not require Rails but will integrate tightly with Rails 3/4/5 to make background processing dead simple.  
+# For Sidekiq Web UI
+require "sidekiq/web"
+mount Sidekiq::Web => "/sidekiq"
