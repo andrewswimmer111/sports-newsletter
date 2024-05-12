77d7f0f0c2ccc05bd43baf7b950bb335e0ad9c49
Alex <akp42@duke.edu>
2024-05-12 15:47:59 -0400

    sidekiq causing odd error

--- ourapp/railsapp/config/routes.rb
+++ ourapp/railsapp/config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   end
 end
 
-# !Note from Alex: Keep this code if you want to work with the Sidekiq Web UI
+# !Note from Alex: add this code if you want to work with the Sidekiq Web UI
 # What is Sidekiq? Sidekiq is a background processing framework for Ruby. It uses threads to handle many jobs at the same time in the same process. It does not require Rails but will integrate tightly with Rails 3/4/5 to make background processing dead simple.  
 # For Sidekiq Web UI
-require "sidekiq/web"
-mount Sidekiq::Web => "/sidekiq"
+# require "sidekiq/web"
+# mount Sidekiq::Web => "/sidekiq"
