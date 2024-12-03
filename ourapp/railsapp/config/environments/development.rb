e2035985e6d2c5e40273006a7d08bf7fb09f8301
Andy Chen <andy.chen@duke.edu>
2024-12-03 14:48:42 -0500

    using duke smtp

--- ourapp/railsapp/config/environments/development.rb
+++ ourapp/railsapp/config/environments/development.rb
@@ -48,19 +48,16 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
 
   # !Mailer:
-  logger.info "SMTP Address: #{ENV['SMTP_ADDRESS']}"
-  logger.info "SMTP Port: #{ENV['SMTP_PORT']}"
-  logger.info "SMTP Username: #{ENV['SMTP_USERNAME']}"
+  # logger.info "SMTP Address: #{ENV['SMTP_ADDRESS']}"
+  # logger.info "SMTP Port: #{ENV['SMTP_PORT']}"
+  # logger.info "SMTP Username: #{ENV['SMTP_USERNAME']}"
   
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              ENV['SMTP_ADDRESS'],
-    port:                 ENV['SMTP_PORT'],
-    domain:               'vcm-44519.vm.duke.edu',
-    user_name:            ENV['SMTP_USERNAME'],
-    password:             ENV['SMTP_PASSWORD'],
-    authentication:       'plain',
-    enable_starttls_auto: true
+    :address => "smtp.duke.edu",
+    :port => 25,
+    :domain => "duke.edu",
+    :enable_starttls_auto => false
   }
 
   
--- ourapp/railsapp/config/environments/production.rb
+++ ourapp/railsapp/config/environments/production.rb
@@ -106,13 +106,10 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              ENV['SMTP_ADDRESS'],
-    port:                 ENV['SMTP_PORT'],
-    domain:               'vcm-44519.vm.duke.edu',
-    user_name:            ENV['SMTP_USERNAME'],
-    password:             ENV['SMTP_PASSWORD'],
-    authentication:       'plain',
-    enable_starttls_auto: true
+    :address => "smtp.duke.edu",
+    :port => 25,
+    :domain => "duke.edu",
+    :enable_starttls_auto => false
   }
 
 
