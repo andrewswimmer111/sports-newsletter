eebf4da23faffaa0e814c3a3cc3e18be0ac765af
Andrew Li <andrew.li@duke.edu>
2025-07-30 09:02:42 -0400

    CORS updated again

--- railsapp/config/initializers/cors.rb
+++ railsapp/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'vcm-48060.vm.duke.edu:3001', 'localhost:3001', 'https://2ff3c511ba16.ngrok-free.app'
+    origins 'http://vcm-48060.vm.duke.edu:3001', 'localhost:3001', 'https://94990e2b2d39.ngrok-free.app'
     # origins 'vcm-43917.vm.duke.edu:3001', 'vcm-43917.vm.duke.edu', 'https://api.replicate.com', 'alexskeleton.colab.duke.edu', "alexskeleton.colab.duke.edu:3001", "alexskeleton.colab.duke.edu:3000" # Replace with your frontend URL or '*' for any origin
     resource '*',
       headers: :any,
